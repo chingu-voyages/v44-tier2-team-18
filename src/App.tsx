91ca31d097a8449fd6aa04b84c2f92fef58396b4
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-05-08 19:13:35 +0200

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>App Page</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
