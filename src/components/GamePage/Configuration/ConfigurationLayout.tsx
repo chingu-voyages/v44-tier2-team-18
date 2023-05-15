75db55be0cbb962894316397dea7f3e3165a4fda
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-05-15 22:08:01 +0200

    created configuration comp

--- src/components/GamePage/Configuration/ConfigurationLayout.tsx
+++ src/components/GamePage/Configuration/ConfigurationLayout.tsx
@@ -1,8 +1,25 @@
-import './ConfigurationLayout.scss'
+import "./ConfigurationLayout.scss";
+import GlobalButton from "./GlobalButton";
 
 function ConfigurationLayout() {
-
-    return <div className="configuration-container">Configuration Panel</div>;
+  return (
+    <div className="configuration-container">
+      <div>
+        <h2>Finish setting up the configuration</h2>
+        <div className="rule-explanation">Learn more about the rules</div>
+      </div>
+      <div className="configuration-content">
+        <div>
+          <GlobalButton buttonText="bot 1 setting" />
+        </div>
+        <div>
+          <GlobalButton buttonText="bot 2 setting" />
+        </div>
+        <div className="speed-slider-title">choose the speed</div>
+      </div>
+      <button className="buttle-button">Battle</button>
+    </div>
+  );
 }
 
 export default ConfigurationLayout;
