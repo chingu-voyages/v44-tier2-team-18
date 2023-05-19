2b22612ebe2d63815b9980aa1cd24f7018a911f4
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-19 19:02:59 +0100

    feat: add bot image to leaderboard

--- src/components/GamePage/Leadboard/Leadboard.tsx
+++ src/components/GamePage/Leadboard/Leadboard.tsx
@@ -1,10 +1,22 @@
 import './Leadboard.scss'
-
+import { botImg } from './assets';
 function Leadboard() {
 
     return (
         <div className="board-container">
-<h3>Battle Results</h3>
+             <h3>Battle Results</h3>
+            <div className='result-container'>
+                      <div className='bot-container'>
+                    <div>
+                        <p>Bot 1</p>
+                          <img src={botImg} alt="bot-1" />
+                  </div>
+                    <div>
+                        <p> Bot 2</p>
+                          <img src={botImg} alt="bot-2" />
+                  </div>
+                </div>
+</div>
     </div>
     )
 }
