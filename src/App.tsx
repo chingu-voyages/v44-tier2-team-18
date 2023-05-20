f76f47a65769fa67c2b68c9e5cd006c639cd1778
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-20 05:43:08 +0100

    fix: add router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import GamePageLayout from "./components/GamePage/GamePageLayout";
-// import WelcomePage from "./components/WelcomePage/WelcomePage";
-// import LearnMorePage from "./components/LearnMorePage/LearnMorePage";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
+import LearnMorePage from "./components/LearnMorePage/LearnMorePage";
 
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <WelcomePage />
-  //   },
-  //   {
-  //     path: "/gamepage",
-  //     element: <GamePageLayout />,
-  //   },
-  //   {
-  //     path: "/learn-more",
-  //     element: <LearnMorePage />,
-  //   }
-  // ])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <WelcomePage />
+    },
+    {
+      path: "/gamepage",
+      element: <GamePageLayout />,
+    },
+    {
+      path: "/learn-more",
+      element: <LearnMorePage />,
+    }
+  ])
   return (
-    // <RouterProvider router={router} />
-    <GamePageLayout/>
+    <RouterProvider router={router} />
+   
   );
 }
 
