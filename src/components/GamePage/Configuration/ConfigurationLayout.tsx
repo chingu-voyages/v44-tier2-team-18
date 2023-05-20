5adb04f3247d8e0a31704e2fc707c6b4260e4eb3
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-05-20 21:26:05 +0200

    created git conig settings

--- src/components/GamePage/Configuration/ConfigurationLayout.tsx
+++ src/components/GamePage/Configuration/ConfigurationLayout.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./ConfigurationLayout.scss";
-import GlobalButton from "./GlobalButton";
+import { SetupBotConfig } from "./SetupBotConfig";
 
 function ConfigurationLayout() {
-  // const setTest = (val: string) => {
-  //   console.log(val);
-  // };
+  const [speed, setSpeed] = useState<string>("1");
+  const [isEditingConfig, setIsEditingConfig] = useState<boolean>(false);
+  const [currentEditingBot, setCurrentEditingBot] = useState<string>("");
+
+  const setEditingBot = (
+    event: React.MouseEvent<HTMLElement>,
+    botName: string
+  ) => {
+    setCurrentEditingBot(botName);
+    setIsEditingConfig(true);
+  };
+
   return (
     <div className="configuration-container">
       <div>
@@ -12,31 +22,56 @@ function ConfigurationLayout() {
         <div className="rule-explanation">Learn more about the rules</div>
       </div>
       <div className="configuration-content">
-        <div>
-          <GlobalButton buttonText="bot 1 setting" />
-        </div>
-        <div>
-          <GlobalButton buttonText="bot 2 setting" />
-        </div>
-        <div className="range-wrapper">
-          <label htmlFor="volume">choose the speed</label>
-          <input
-            className="range-bar"
-            type="range"
-            id="volume"
-            name="volume"
-            min="1"
-            max="4"
-          />
-          <div className="range-value">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
+        {!isEditingConfig && (
+          <div className="not-editing">
+            <div className="button-wrapper">
+              <button
+                onClick={(e: React.MouseEvent<HTMLElement>) =>
+                  setEditingBot(e, "Bot1")
+                }
+                className="globalButton">
+                Bot1 Setting
+              </button>
+              {/* <GlobalButton buttonText="bot 1 setting" /> */}
+            </div>
+            <div className="button-wrapper">
+              <button className="globalButton">Bot2 Setting</button>
+              {/* <GlobalButton buttonText="bot 2 setting" /> */}
+            </div>
+            <div className="range-wrapper">
+              <label htmlFor="speed">choose the speed</label>
+              <input
+                className="range-bar"
+                type="range"
+                id="speed"
+                name="speed"
+                min="1"
+                max="4"
+                value={speed}
+                onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                  setSpeed(e.currentTarget.value)
+                }
+              />
+              <div className="range-value">
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+              </div>
+              <div className="bot-each-config">Operation:</div>
+            </div>
           </div>
-        </div>
+        )}
+        {isEditingConfig && (
+          <>
+            <SetupBotConfig
+              currentEditingBot={currentEditingBot}
+              setIsEditingConfig={setIsEditingConfig}
+            />
+          </>
+        )}
       </div>
-      <button className="buttle-button">Battle</button>
+      {!isEditingConfig && <button className="buttle-button">Battle</button>}
     </div>
   );
 }
