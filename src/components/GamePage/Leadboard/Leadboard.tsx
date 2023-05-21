d23d51cd13d1827550bcc89e3ccc67cf3ea3c6e0
Your Name <lebomariri505@gmail.com>
2023-05-21 19:07:16 +0200

    Leaderboard updated

--- src/components/GamePage/Leadboard/Leadboard.tsx
+++ src/components/GamePage/Leadboard/Leadboard.tsx
@@ -2,7 +2,27 @@ import './Leadboard.scss'
 
 function Leadboard() {
 
-    return <div className="board-container">Table of results</div>;
+    return <div className="board-container">
+        <div className='title'>
+            <h3>Bot results</h3>
+        </div >
+        <div className='botname'>
+        <div className='botname-A'>
+            <p>Bot A</p>
+            
+
+        </div>
+        <div>
+            <p>Winner</p>
+            {/* Functional code that shows the winner of the round after the arena battle */}
+        </div>
+        <div className='botname-B'>
+            
+            <p>Bot B</p>
+
+        </div>
+        </div>
+    </div>;
 }
 
 export default Leadboard;
