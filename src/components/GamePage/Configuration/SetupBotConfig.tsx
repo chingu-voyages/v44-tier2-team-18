98d870d1644503154f81b266b15e247081a664a2
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-05-28 12:24:47 +0200

    created the violation to prevent duplicate bot name

--- src/components/GamePage/Configuration/SetupBotConfig.tsx
+++ src/components/GamePage/Configuration/SetupBotConfig.tsx
@@ -13,6 +13,8 @@ export const SetupBotConfig = (props: Props) => {
   const bot1Config = useAppSelector((state) => state.bot1Config);
   const bot2Config = useAppSelector((state) => state.bot2Config);
 
+  const [isBotNameDuplicated, setIsBotNameDuplicated] = useState(false);
+
   const setSelectValues = () => {
     const booleanValueElem = document.querySelector(
       ".booleanValue"
@@ -37,11 +39,24 @@ export const SetupBotConfig = (props: Props) => {
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
+
     const target = e.target as typeof e.target & {
       botName: { value: string };
       booleanValue: { value: number };
       startingDirection: { value: string };
     };
+
+    const theOtherBotName =
+      props.currentEditingBot === "Bot1"
+        ? bot2Config.botName
+        : bot1Config.botName;
+    if (theOtherBotName && target.botName.value === theOtherBotName) {
+      setIsBotNameDuplicated(true);
+      return;
+    }
+
+    setIsBotNameDuplicated(false);
+
     dispatch(
       props.currentEditingBot === "Bot1"
         ? configActions.setBot1Config({
@@ -55,6 +70,7 @@ export const SetupBotConfig = (props: Props) => {
             startingDirection: target.startingDirection.value,
           })
     );
+
     props.setCurrentEditingBot("");
   };
 
@@ -75,6 +91,7 @@ export const SetupBotConfig = (props: Props) => {
                 name="botName"
                 id="botName"
                 type="text"
+                className={`botname ${isBotNameDuplicated ? " alert" : ""}`}
                 defaultValue={
                   props.currentEditingBot === "Bot1"
                     ? bot1Config.botName
@@ -82,6 +99,10 @@ export const SetupBotConfig = (props: Props) => {
                 }
               />
             </label>
+            <div className="alert-text">
+              {isBotNameDuplicated &&
+                "Bot Name is duplicated. Change the bot name."}
+            </div>
           </div>
           <div className="bot-each-config">
             <label>
