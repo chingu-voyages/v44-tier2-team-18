5fee42f54c4d55ea28c11d757743779f6e5bbcb8
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-06-01 13:43:51 +0200

    modified styles to be responsibe

--- src/components/GamePage/GamePageLayout.tsx
+++ src/components/GamePage/GamePageLayout.tsx
@@ -2,28 +2,51 @@ import React, { createContext } from "react";
 import Arena from "./Arena/Arena";
 import ConfigurationLayout from "./Configuration/ConfigurationLayout";
 import Leadboard from "./Leadboard/Leadboard";
-import './GamePageLayout.scss'
-// import { boardDefault } from "./Constant";
-
-// export const AppContext = createContext<any>({});
+import "./GamePageLayout.scss";
+import useScreenSize from "../../hooks/useScreenSize";
 
 function GamePageLayout(): JSX.Element {
-    // const [board, setBoard] = useState(boardDefault);
-    return (
-        <div className="layout-container">
-            {/* <AppContext.Provider value={{ board, setBoard }}> */}
-            <div className="configuraion-section">
-                <ConfigurationLayout />
-            </div>
+  const { browserInnerWidthSize } = useScreenSize();
+  console.log(browserInnerWidthSize);
+
+  return (
+    <>
+      <div className="game-page">
+        {browserInnerWidthSize <= 820 && (
+          <div className="layout-container mobile-size">
+            {/* will use it later */}
+            {/* <div className="configuraion-section">
+            <ConfigurationLayout />
+          </div> */}
+
             <div className="leadboard-section">
-                <Leadboard />
+              <Leadboard />
             </div>
             <div className="arena-section">
-                <Arena />
+              <Arena />
             </div>
-            {/* </AppContext.Provider> */}
-        </div>
-    )
+          </div>
+        )}
+        {browserInnerWidthSize > 820 && (
+          <div className="layout-container pc-size">
+            <div className="section-left">
+              <div className="configuraion-section">
+                <ConfigurationLayout />
+              </div>
+            </div>
+            <div className="section-right">
+              <div className="leadboard-section">
+                <Leadboard />
+              </div>
+              <div className="arena-section">
+                <Arena />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default GamePageLayout;
