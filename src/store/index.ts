c6dba6b692e258f446084ee6961123f6de501007
giangpham-cfg <giang.pham1610@gmail.com>
2023-06-02 12:50:17 +0200

    style: Update style of GamePageLayout

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,12 +8,10 @@ export type eachBotConfig = {
   colour: string;
   booleanValue: string;
   startingDirection: string;
-  // result: number;
 };
 type configContextObj = {
   speed: number;
   operation: string;
-  // output: number | null;
   bot1Config: eachBotConfig;
   bot2Config: eachBotConfig;
 };
@@ -21,7 +19,6 @@ type configContextObj = {
 const initialState: configContextObj = {
   speed: 1,
   operation: "",
-  // output: null,
   bot1Config: {
     botName: "",
     position: [0, 0],
@@ -29,7 +26,6 @@ const initialState: configContextObj = {
     colour: "Red",
     booleanValue: "",
     startingDirection: "",
-    // result: 0
   },
   bot2Config: {
     botName: "",
@@ -38,7 +34,6 @@ const initialState: configContextObj = {
     colour: "Blue",
     booleanValue: "",
     startingDirection: "",
-    // result: 0
   },
 };
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -56,15 +51,10 @@ const botConfigSlice = createSlice({
   reducers: {
     setSpeed: (state, action) => {
       state.speed = action.payload;
-      // console.log("check action", action)
     },
     setOperation(state, action) {
       state.operation = action.payload;
-      // console.log('check action', action)
     },
-    // setOutput(state, action) {
-    //   state.output = action.payload;
-    // },
     setBot1Config(state, action) {
       if (action.payload.botName) {
         state.bot1Config.botName = action.payload.botName;
@@ -81,9 +71,6 @@ const botConfigSlice = createSlice({
       if (action.payload.active) {
         state.bot1Config.active = action.payload.active;
       }
-      // if (action.payload.result) {
-      //   state.bot1Config.result = action.payload.result;
-      // }
     },
     setBot2Config(state, action) {
       if (action.payload.botName) {
@@ -101,9 +88,6 @@ const botConfigSlice = createSlice({
       if (action.payload.active) {
         state.bot2Config.active = action.payload.active;
       }
-      // if (action.payload.result) {
-      //   state.bot2Config.result = action.payload.result;
-      // }
     },
   },
 });
